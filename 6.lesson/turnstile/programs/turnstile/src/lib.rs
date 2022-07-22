7d1980055865249e46368c24817ecd653d1de635
Marketa <markettka@email.cz>
2022-07-22 17:28:33 +0200

    create coin and push function and test it

--- 6.lesson/turnstile/programs/turnstile/src/lib.rs
+++ 6.lesson/turnstile/programs/turnstile/src/lib.rs
@@ -9,16 +9,38 @@ pub mod turnstile {
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         let state = &mut ctx.accounts.state;
         state.locked = true;
+        Ok(())
+    }
 
+    pub fn coin(ctx: Context<UpdateState>) -> Result<()> {
+        let state = &mut ctx.accounts.state;
+        state.locked = false;
+        Ok(())
+    }
+
+    pub fn push(ctx: Context<UpdateState>) -> Result<()> {
+        let state = &mut ctx.accounts.state;
+        state.locked = true;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
+    #[account(
+        init,
+        payer = user,
+        space = 8 + 1 // 8 for discriminator
+    )]
     pub state: Account<'info, State>,
+    #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info,System>
+}
+
+#[derive(Accounts)]
+pub struct UpdateState<'info> {
+    pub state: Account<'info, State>,
 }
 
 #[account]
